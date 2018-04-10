612c1c2476ce57923e433794055feec4b00d098c
ck-unifr <chenk.ck03@gmail.com>
2018-04-10 09:14:05 +0200

    update my solutions

--- product-description-generation.py
+++ product-description-generation.py
@@ -105,10 +105,10 @@ for file_name in dirs:
 #     print(product_id)
 
 
-prepare_img_data = False
-prepare_text_data = False
+prepare_img_data = True
+prepare_text_data = True
 
-TRAIN_MODEL = False
+TRAIN_MODEL = True
 EVALUATE_MODEL = True
 
 # ---------
@@ -116,6 +116,7 @@ EVALUATE_MODEL = True
 
 # img_width, img_height = 80, 80
 # img_width, img_height = 100, 100
+img_width, img_height = 224, 224
 
 # img_dir_path = "data/images_{}_{}/".format(img_width, img_height)
 img_dir_path = "data/images/"
@@ -141,6 +142,8 @@ def extract_features(dict_product_img):
 
 
 if prepare_img_data:
+    print('Prepare image data ...')
+
     # key: product id, value: image
     dict_product_img = dict()
 
@@ -148,7 +151,7 @@ if prepare_img_data:
         file_path = os.path.join(img_dir_path, file_name)
 
         # img = load_img(file_path)         # this is a PIL image
-        img = load_img(file_path, target_size=(224, 224))   # this is a PIL image
+        img = load_img(file_path, target_size=(img_width, img_height))   # this is a PIL image
         x = img_to_array(img)                               # this is a Numpy array with shape (img_width, img_height, 3)
         x = x.reshape((1, x.shape[0], x.shape[1], x.shape[2]))
         # x = x.reshape((1,) + x.shape)   # this is a Numpy array with shape (1, 3, img_width, img_height)
@@ -169,14 +172,68 @@ if prepare_img_data:
     # save features to file
     dump(dict_product_img_features, open(product_image_feature_file_path, 'wb'))
 
+    print('save product image features to {}'.format(product_image_feature_file_path))
+
 
 # --------------
 # Prepare Text Data
 
 product_description_file_path = 'product-descriptions.txt'
 
+# https://machinelearningmastery.com/develop-a-deep-learning-caption-generation-model-in-python/
+# Convert all words to lowercase.
+# Remove all punctuation.
+# Remove all words that are one character or less in length (e.g. ‘a’).
+# Remove all words with numbers in them.
+def clean_descriptions(descriptions):
+    # prepare translation table for removing punctuation
+    table = str.maketrans('', '', string.punctuation)
+    for key, desc in descriptions.items():
+        # tokenize
+        desc = desc.split()
+        # convert to lower case
+        desc = [word.lower() for word in desc]
+        # remove punctuation from each token
+        desc = [w.translate(table) for w in desc]
+        # remove hanging 's' and 'a'
+        desc = [word for word in desc if len(word) > 1]
+        # remove tokens with numbers in them
+        desc = [word for word in desc if word.isalpha()]
+        # store as string
+        clean_str = ' '.join(desc)
+        if not clean_str:
+            print('cleaned description of product {} is empty'.format(key))
+        else:
+            descriptions[key] = clean_str
+
+# convert the loaded descriptions into a vocabulary of words
+def to_vocabulary(descriptions):
+    # build a list of all description strings
+    all_desc = set()
+    for key in descriptions.keys():
+        [all_desc.update(d.split()) for d in descriptions[key]]
+    return all_desc
+
+
+# save descriptions to file, one per line
+def save_descriptions(descriptions, filename):
+    lines = list()
+    for key, desc in descriptions.items():
+        if not desc:
+            print('product {} does not have a description'.format(key))
+        # print(key)
+        # print(desc)
+        lines.append(str(key) + ' ' + desc)
+    data = '\n'.join(lines)
+    file = open(filename, 'w')
+    file.write(data)
+    file.close()
+
+
 # Create a dictionary, key: product id -> value: description
 if prepare_text_data:
+    print('Prepare text data ...')
+
     dict_product_des = dict()
     for product_id in list_product_id:
         # we assume that one product has only one description.
@@ -191,67 +248,14 @@ if prepare_text_data:
     for product_id in list_product_id[0:5]:
         print(dict_product_des[product_id])
 
-    # https://machinelearningmastery.com/develop-a-deep-learning-caption-generation-model-in-python/
-    # Convert all words to lowercase.
-    # Remove all punctuation.
-    # Remove all words that are one character or less in length (e.g. ‘a’).
-    # Remove all words with numbers in them.
-    def clean_descriptions(descriptions):
-        # prepare translation table for removing punctuation
-        table = str.maketrans('', '', string.punctuation)
-        for key, desc in descriptions.items():
-            # tokenize
-            desc = desc.split()
-            # convert to lower case
-            desc = [word.lower() for word in desc]
-            # remove punctuation from each token
-            desc = [w.translate(table) for w in desc]
-            # remove hanging 's' and 'a'
-            desc = [word for word in desc if len(word) > 1]
-            # remove tokens with numbers in them
-            desc = [word for word in desc if word.isalpha()]
-            # store as string
-            clean_str = ' '.join(desc)
-            if not clean_str:
-                print('cleaned description of product {} is empty'.format(key))
-            else:
-                descriptions[key] = clean_str
-
-
     # clean descriptions
     clean_descriptions(dict_product_des)
     for product_id in list_product_id[0:5]:
         print(dict_product_des[product_id])
 
-
-    # convert the loaded descriptions into a vocabulary of words
-    def to_vocabulary(descriptions):
-        # build a list of all description strings
-        all_desc = set()
-        for key in descriptions.keys():
-            [all_desc.update(d.split()) for d in descriptions[key]]
-        return all_desc
-
-
     # summarize vocabulary
     vocabulary = to_vocabulary(dict_product_des)
     print('Vocabulary Size: %d' % len(vocabulary))
-
-
-    # save descriptions to file, one per line
-    def save_descriptions(descriptions, filename):
-        lines = list()
-        for key, desc in descriptions.items():
-            if not desc:
-                print('product {} does not have a description'.format(key))
-            # print(key)
-            # print(desc)
-            lines.append(str(key) + ' ' + desc)
-        data = '\n'.join(lines)
-        file = open(filename, 'w')
-        file.write(data)
-        file.close()
-
 
     # save descriptions
     save_descriptions(dict_product_des, product_description_file_path)
@@ -318,6 +322,7 @@ def to_lines(descriptions):
         [all_desc.append(d) for d in descriptions[key]]
     return all_desc
 
+
 # fit a tokenizer given caption descriptions
 def create_tokenizer(descriptions):
     lines = to_lines(descriptions)
@@ -375,27 +380,34 @@ def data_generator(descriptions, photos, tokenizer, max_length):
                 in_img, in_seq, out_word = create_sequences(tokenizer, max_length, desc_list, photo)
                 yield [[in_img, in_seq], out_word]
 
+
+
 # --------------
 # Define the model
 
 # define the captioning model
 def define_model(vocab_size, max_length):
+
     # feature extractor model
     inputs1 = Input(shape=(4096,))
     fe1 = Dropout(0.5)(inputs1)
     fe2 = Dense(256, activation='relu')(fe1)
+
     # sequence model
     inputs2 = Input(shape=(max_length,))
     se1 = Embedding(vocab_size, 256, mask_zero=True)(inputs2)
     se2 = Dropout(0.5)(se1)
     se3 = LSTM(256)(se2)
+
     # decoder model
     decoder1 = add([fe2, se3])
     decoder2 = Dense(256, activation='relu')(decoder1)
     outputs = Dense(vocab_size, activation='softmax')(decoder2)
+
     # tie it together [image, seq] [word]
     model = Model(inputs=[inputs1, inputs2], outputs=outputs)
     model.compile(loss='categorical_crossentropy', optimizer='adam')
+
     # summarize model
     print(model.summary())
     # plot_model(model, to_file='model.png', show_shapes=True)
@@ -418,7 +430,7 @@ print('Descriptions: %d' % len(all_descriptions))
 #     print(value)
 
 # prepare train and test sets
-percentage_train = 0.2
+percentage_train = 0.8
 list_train_product_id = list_product_id[0:int(len(list_product_id)*percentage_train)]
 list_test_product_id = list_product_id[len(list_train_product_id):]
 
@@ -459,6 +471,7 @@ model = define_model(vocab_size, max_length)
 
 
 if TRAIN_MODEL:
+    print('Train model ... ')
     # train the model, run epochs manually and save after each epoch
     epochs = 2
     steps = len(train_descriptions)
@@ -530,6 +543,7 @@ def evaluate_model(model, descriptions, photos, tokenizer, max_length):
 
 # prepare test set
 if EVALUATE_MODEL:
+    print('Evaluate model ...')
     # load the model
     # filename = 'model-ep002-loss3.245-val_loss3.612.h5'
     filename = 'model_1.h5'
